689214cfea461cdf3fe359027a6d89f1e6b90f3f
Joshua Willis <joshua.m.willis8@gmail.com>
2023-09-07 23:14:26 -0700

    Update IDataSource.cs
    Remove redundant access modifiers from public interface

--- TrackSpendMvvm.Data/Interfaces/IDataSource.cs
+++ TrackSpendMvvm.Data/Interfaces/IDataSource.cs
@@ -6,12 +6,12 @@ public interface IDataSource<TEntity, in TKey> : IEnumerable<TEntity>
     where TEntity : class
     where TKey : IEquatable<TKey>
 {
-    public Task<TEntity?> GetAsync(TKey key, CancellationToken cancellationToken = default);
-    public Task<IEnumerable<TEntity>> GetAllAsync(int count = 0, int skip = 0, CancellationToken cancellationToken = default);
-    public Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
-    public Task<IEnumerable<TEntity>> FilterAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+    Task<TEntity?> GetAsync(TKey key, CancellationToken cancellationToken = default);
+    Task<IEnumerable<TEntity>> GetAllAsync(int count = 0, int skip = 0, CancellationToken cancellationToken = default);
+    Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+    Task<IEnumerable<TEntity>> FilterAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
     
-    public void Add(TEntity entity);
-    public void Update(TEntity entity);
-    public bool Remove(TEntity entity);
+    void Add(TEntity entity);
+    void Update(TEntity entity);
+    bool Remove(TEntity entity);
 }
